7b3e1a242eabf59571399f643b3732503d3d39e0
michael <zdong2010@gmail.com>
2020-01-18 16:49:13 +0800

    SizedBox固定尺寸的盒子

--- lib/demo/layout_widget.dart
+++ lib/demo/layout_widget.dart
@@ -4,13 +4,39 @@ class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        // children: <Widget>[
+        //   IconBadge(Icons.pool),
+        //   IconBadge(Icons.beach_access, size: 64),
+        //   IconBadge(Icons.airplanemode_active)
+        // ]
         children: <Widget>[
-          IconBadge(Icons.pool),
-          IconBadge(Icons.beach_access),
-          IconBadge(Icons.airplanemode_active)
-        ]
+          SizedBox(
+            width: 200.0,
+            height: 300.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0)
+              ),
+              child: Icon(Icons.ac_unit, color: Colors.white,size: 32.0),
+            ) ,
+          ),
+          SizedBox(height: 32.0),
+          SizedBox(
+            width: 100.0,
+            height: 100.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0)
+              ),
+              child: Icon(Icons.brightness_2, color: Colors.white,size: 32.0),
+            ) ,
+          )
+        ],
         ) ,
       );
   }
